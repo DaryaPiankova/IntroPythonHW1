68ac7d7aa2c0a541a4b17ddc4f507800df73c3ea
Daria <darya.piankova@yandex.ru>
2023-04-11 21:22:16 +0500

    task3

--- hw1.py
+++ hw1.py
@@ -1,8 +1,13 @@
-a = int(input('Введите трехзначное чило: '))
-while (a < 100 or a > 999):
-    a = int(input('Неверно! Введите трехзначное чило: '))
-firstNumber = a//100
-secondNumber = a//10 % 10
-thirdNumber = a % 100 % 10
-print(firstNumber+secondNumber+thirdNumber,
-      '(', firstNumber, '+', secondNumber, '+', thirdNumber, ')')
+a = int(input('Введите  номер билета: '))
+while a < 100000 or a > 999999:
+    a = int(input('Неверно! Номер билета - шестизначное число! Введите  номер билета: '))
+firstNumber = a//100000
+secondNumber = a // 10000 % 10
+thirdNumber = a//1000 % 10
+fourthNumber = a//100 % 10
+fifthNumber = a//10 % 10
+sixthNumber = a % 10
+if firstNumber+secondNumber+thirdNumber == fourthNumber+fifthNumber+sixthNumber:
+    print('yes')
+else:
+    print('no')
