1baf24656848c8440ded749d0f6fb5845acf7287
Daria <darya.piankova@yandex.ru>
2023-04-11 21:26:37 +0500

    task2

--- hw1.py
+++ hw1.py
@@ -1,8 +1,1 @@
-a = int(input('Введите трехзначное чило: '))
-while (a < 100 or a > 999):
-    a = int(input('Неверно! Введите трехзначное чило: '))
-firstNumber = a//100
-secondNumber = a//10 % 10
-thirdNumber = a % 100 % 10
-print(firstNumber+secondNumber+thirdNumber,
-      '(', firstNumber, '+', secondNumber, '+', thirdNumber, ')')
+
